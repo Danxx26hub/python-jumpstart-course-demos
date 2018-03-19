959d58dfd31b0bf041694ffc7b011ba98234c705
Daniel <danxx26@gmail.com>
2018-03-19 04:31:08 +0000

    added my attempt at the hello world app

---
+++ apps/01_hello_world/you_try/helloworld.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python 
+
+
+print('_' * 50)
+print('             Hello World!  ')
+print('_' * 50)
+print()
+user_text = input('what is your name\n')
+
+print(f'Nice to meet you {user_text}!')
+
+
